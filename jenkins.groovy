92ab8d7250a353584eb92069b210e003e8e0d98f
rinatleb <lebedev_rinat@mail.ru>
2024-07-01 23:34:15 +0300

    edit jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,7 +1,7 @@
 task_branch = "${TEST_BRANCH_NAME}"
 def branch_cutted = task_branch.contains("all") ? task_branch.split('/')[1] : task_branch.trim()
 currentBuild.displayName = "$branch_cutted"
-base_git_url = "https://gitlab.com/rinatlebedev1/binance_qa_automation_tests.git"
+base_git_url = "https://github.com/RinatLebedev/binance_qa_automation_tests.git"
 
 
 node {
