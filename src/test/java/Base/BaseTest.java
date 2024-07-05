5eaeb738617a8467d57b0fff328d7e872f224596
rinatleb <lebedev_rinat@mail.ru>
2024-07-05 14:36:31 +0300

    edit basetest

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -25,7 +25,7 @@ abstract public class BaseTest implements Variables{
 
     public void setUp(){
         WebDriverManager.chromedriver().setup();
-        Configuration.browser = "chrome";
+        //Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager"; //Должен ли драйвер подождать, пока страница полностью загрузится
         Configuration.headless = false; //Отображется ли окно при прогонке теста(false). Безоконный режим - (true)
@@ -33,12 +33,14 @@ abstract public class BaseTest implements Variables{
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         //Selenoid
-        Configuration.baseUrl = "http://localhost:4444/wd/hub";
+        //Configuration.baseUrl = "http://localhost:4444/wd/hub";
         Configuration.browserCapabilities = capabilities();
+        Configuration.remote = "http://localhost:4444/";
 
 
     }
-    static DesiredCapabilities capabilities() {
+
+    public static DesiredCapabilities capabilities() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName("chrome");
         capabilities.setCapability("selenoid:options", ImmutableMap.of(
